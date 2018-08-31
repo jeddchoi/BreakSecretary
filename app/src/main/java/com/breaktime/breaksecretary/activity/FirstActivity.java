69fb699e7816b56f422cdc52a70b5cb512e8560d
jeddchoi <luv.gwangyoung@gmail.com>
2018-08-31 21:11:37 +0900

    Google Login applied

--- app/src/main/java/com/breaktime/breaksecretary/activity/FirstActivity.java
+++ app/src/main/java/com/breaktime/breaksecretary/activity/FirstActivity.java
@@ -3,53 +3,49 @@ package com.breaktime.breaksecretary.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.media.MediaPlayer;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
 import com.breaktime.breaksecretary.R;
-import com.breaktime.breaksecretary.receiver.TokenBroadcastReceiver;
-import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.gms.tasks.TaskCompletionSource;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.kakao.auth.ISessionCallback;
-import com.kakao.auth.Session;
-import com.kakao.util.exception.KakaoException;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.yqritc.scalablevideoview.ScalableType;
 import com.yqritc.scalablevideoview.ScalableVideoView;
 
-import org.json.JSONObject;
-
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+
 
 public class FirstActivity extends AppCompatActivity {
     public static Context mContext;
     private String TAG = "FirstActivity";
+    private static final int RC_SIGN_IN = 9001;
+
     private ScalableVideoView mVideoView;
     private Button signupButton;
-    private Button kakaoLoginButton;
     private Button loginButton;
-
+    private SignInButton googleSigninButton;
     private FirebaseAuth mAuth;
-    private String mCustomToken;
-    private TokenBroadcastReceiver mTokenReceiver;
+
+    private GoogleSignInClient mGoogleSignInClient;
+//    private Button kakaoLoginButton;
+//    private String mCustomToken;
+//    private TokenBroadcastReceiver mTokenReceiver;
 
 
     @Override
@@ -57,6 +53,15 @@ public class FirstActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
         mContext = this;
+
+        mAuth = FirebaseAuth.getInstance();
+        if (mAuth.getCurrentUser() != null) {
+            // User is signed in (getCurrentUser() will be null if not signed in)
+            Intent intent = new Intent(mContext, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            startActivity(intent);
+            finish();
+        }
 
         Log.d(TAG, "FirstActivity onCreate");
         mVideoView = (ScalableVideoView) findViewById(R.id.video_view);
@@ -78,8 +83,6 @@ public class FirstActivity extends AppCompatActivity {
             Log.e(TAG, "mVideoView Error");
         }
 
-
-
         signupButton = findViewById(R.id.signupButton);
         signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -88,6 +91,23 @@ public class FirstActivity extends AppCompatActivity {
                 Intent intent = new Intent(mContext, SignUpActivity.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
                 startActivity(intent);
+            }
+        });
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        googleSigninButton = findViewById(R.id.googleSigninButton);
+        googleSigninButton.setSize(SignInButton.SIZE_WIDE);
+        googleSigninButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
             }
         });
 
@@ -101,180 +121,153 @@ public class FirstActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        kakaoLoginButton = findViewById(R.id.kakaoLoginButton);
-        kakaoLoginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startSignIn();
-            }
-        });
 
-        // Create token receiver (for demo purposes only)
-        mTokenReceiver = new TokenBroadcastReceiver() {
-            @Override
-            public void onNewToken(String token) {
-                Log.d(TAG, "onNewToken:" + token);
-                setCustomToken(token);
-            }
-        };
 
-        mAuth = FirebaseAuth.getInstance();
+
+
+
+        /* Kakao Login */
+
+//        kakaoLoginButton = findViewById(R.id.kakaoLoginButton);
+//        kakaoLoginButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Log.d(TAG, "kakao login button clicked");
+//                Intent intent = new Intent(mContext, KakaoLoginActivity.class);
+//                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        // Create token receiver (for demo purposes only)
+//        mTokenReceiver = new TokenBroadcastReceiver() {
+//            @Override
+//            public void onNewToken(String token) {
+//                Log.d(TAG, "onNewToken:" + token);
+//                setCustomToken(token);
+//            }
+//        };
     }
 
 
 
-    /**
-     * OnActivityResult() should be overridden for Kakao Login because Kakao Login uses startActivityForResult().
-     */
+    // [START onactivityresult]
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data);
-    }
 
-    /**
-     *
-     * @param kakaoAccessToken Access token retrieved after successful Kakao Login
-     * @return Task object that will call validation server and retrieve firebase token
-     */
-    private Task<String> getFirebaseJwt(final String kakaoAccessToken) {
-        final TaskCompletionSource<String> source = new TaskCompletionSource<>();
+        Log.d(TAG, "firebaseAuthWithGoogle: onActivityResult");
 
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url = getResources().getString(R.string.validation_server_domain) + "/verifyToken";
-        HashMap<String, String> validationObject = new HashMap<>();
-        validationObject.put("token", kakaoAccessToken);
-
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, new JSONObject(validationObject), new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-                try {
-                    String firebaseToken = response.getString("firebase_token");
-                    source.setResult(firebaseToken);
-                } catch (Exception e) {
-                    source.setException(e);
-                }
-
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                Log.d(TAG, "firebaseAuthWithGoogle: onActivityResult" + account.getId());
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e);
             }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e(TAG, error.toString());
-                source.setException(error);
-            }
-        }) {
-            @Override
-            protected Map<String, String> getParams() {
-                Map<String, String> params = new HashMap<>();
-                params.put("token", kakaoAccessToken);
-                return params;
-            }
-        };
-
-        queue.add(request);
-        return source.getTask();
+        }
     }
+    // [END onactivityresult]
 
-    // [START on_start_check_user]
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
+    // [START auth_with_google]
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
 
-    }
-    // [END on_start_check_user]
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        registerReceiver(mTokenReceiver, TokenBroadcastReceiver.getFilter());
-    }
-
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        unregisterReceiver(mTokenReceiver);
-    }
-
-    private void startSignIn() {
-        // Initiate sign in with custom token
-        // [START sign_in_custom]
-        mAuth.signInWithCustomToken(mCustomToken)
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "signInWithCustomToken:success");
+                            Log.d(TAG, "signInWithCredential:success");
                             FirebaseUser user = mAuth.getCurrentUser();
-
+                            Intent intent = new Intent(mContext, MainActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                            startActivity(intent);
+                            finish();
                         } else {
                             // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithCustomToken:failure", task.getException());
-                            Toast.makeText(FirstActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Snackbar.make(findViewById(R.id.container), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
                         }
+
                     }
                 });
-        // [END sign_in_custom]
     }
+    // [END auth_with_google]
 
-    private void setCustomToken(String token) {
-        mCustomToken = token;
-
-        String status;
-        if (mCustomToken != null) {
-            status = "Token:" + mCustomToken;
-        } else {
-            status = "Token: null";
-        }
-
-        // Enable/disable sign-in button and show the token
-        findViewById(R.id.button_sign_in).setEnabled((mCustomToken != null));
-        ((TextView) findViewById(R.id.text_token_status)).setText(status);
+    // [START signin]
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
+    // [END signin]
 
 
-    /**
-     * Session callback class for Kakao Login. OnSessionOpened() is called after successful login.
-     */
-    private class KakaoSessionCallback implements ISessionCallback {
-        @Override
-        public void onSessionOpened() {
-            Toast.makeText(getApplicationContext(), "Successfully logged in to Kakao. Now creating or updating a Firebase User.", Toast.LENGTH_LONG).show();
-            String accessToken = Session.getCurrentSession().getAccessToken();
-            getFirebaseJwt(accessToken).continueWithTask(new Continuation<String, Task<AuthResult>>() {
-                @Override
-                public Task<AuthResult> then(@NonNull Task<String> task) throws Exception {
-                    String firebaseToken = task.getResult();
 
-                    return mAuth.signInWithCustomToken(firebaseToken);
-                }
-            }).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-                    if (task.isSuccessful()) {
-                        Intent intent = new Intent(mContext, MainActivity.class);
-                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-                        startActivity(intent);
-                    } else {
-                        Toast.makeText(getApplicationContext(), "Failed to create a Firebase user.", Toast.LENGTH_LONG).show();
-                        if (task.getException() != null) {
-                            Log.e(TAG, task.getException().toString());
-                        }
-                    }
-                }
-            });
-        }
 
-        @Override
-        public void onSessionOpenFailed(KakaoException exception) {
-            if (exception != null) {
-                Log.e(TAG, exception.toString());
-            }
-        }
-    }
+
+
+
+
+/* Kakao Login */
+
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        registerReceiver(mTokenReceiver, TokenBroadcastReceiver.getFilter());
+//    }
+//
+//
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        unregisterReceiver(mTokenReceiver);
+//    }
+//
+//    private void startSignIn() {
+//        // Initiate sign in with custom token
+//        // [START sign_in_custom]
+//        mAuth.signInWithCustomToken(mCustomToken)
+//                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                        if (task.isSuccessful()) {
+//                            // Sign in success, update UI with the signed-in user's information
+//                            Log.d(TAG, "signInWithCustomToken:success");
+//                            FirebaseUser user = mAuth.getCurrentUser();
+//
+//                        } else {
+//                            // If sign in fails, display a message to the user.
+//                            Log.w(TAG, "signInWithCustomToken:failure", task.getException());
+//                            Toast.makeText(FirstActivity.this, "Authentication failed.",
+//                                    Toast.LENGTH_SHORT).show();
+//
+//                        }
+//                    }
+//                });
+//        // [END sign_in_custom]
+//    }
+
+//    private void setCustomToken(String token) {
+//        mCustomToken = token;
+//
+//        String status;
+//        if (mCustomToken != null) {
+//            status = "Token:" + mCustomToken;
+//        } else {
+//            status = "Token: null";
+//        }
+//
+//        // Enable/disable sign-in button and show the token
+//        findViewById(R.id.button_sign_in).setEnabled((mCustomToken != null));
+//        ((TextView) findViewById(R.id.text_token_status)).setText(status);
+//    }
+
 }
